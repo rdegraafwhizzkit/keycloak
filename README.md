9a5d02ab4bc6f1ff5c052321f7a70cb44f7df4ea
Rutger de Graaf <rutger@whizzkit.nl>
2021-05-16 21:52:25 +0200

    Improved README.md, added deleting of realm and client

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# keycloak
+# Keycloak / Databricks SSO Integration
+
+## Optional: build and run the Keycloak Docker image
+```
+cd docker
+./build.sh
+```
+The run.sh script is provided to be used at the first startup.
+
+### Integrate Keycloak with Apache
+You can find a template for the Keycloak virtualhost file in docker/virtualhost.conf.template. 
+Lines containing FIXME need configuration. Use a certificate signed by an authority, 
+self-signed certificates will give trouble, running without SSL is highly discouraged.
+
+## Provision a realm in Keycloak
+Create a file called keycloak.local.yaml as a copy from keycloak.yaml and configure all values. 
+The keycloak_password value should be set to the same value as provided at first run.
+Make sure a realm with the name configure in keycloak_realm does not exist already or else an error 409 will be raised.
+Now run the provisioning script:
+```
+virtualenv venv --python python3
+. venv/bin/activate
+pip install --upgrade pip
+cd provision
+pip install -r requirements.txt
+python keycloak.py
+```
+It will output all necessary values to configure Databricks
+
+## Configure Databricks
+
--- provision/keycloak.py
+++ provision/keycloak.py
@@ -3,6 +3,12 @@ import json
 from pprint import pprint as pp
 from dynaconf import Dynaconf
 
+
+class KeycloakAPI:
+    def __init__(self, settings: Dynaconf):
+        pass
+
+
 s = Dynaconf(
     environments=True,
     settings_file='keycloak.yaml'
@@ -11,11 +17,42 @@ s = Dynaconf(
 databricks_base_url = f'https://{s.databricks_deployment}.cloud.databricks.com'
 databricks_saml_url = f'{databricks_base_url}/saml/consume'
 
+keycloak_api = KeycloakAPI(s)
+
+
+def get_realms():
+    return requests.get(
+        f'{s.keycloak_base_url}/auth/admin/realms',
+        headers={
+            'content-type': 'application/json',
+            'Authorization': f'bearer {token}'
+        }
+    ).json()
+
+
+def get_realm():
+    return [realm for realm in get_realms() if s.keycloak_realm == realm['realm']]
+
+
+def get_clients():
+    return requests.get(
+        f'{s.keycloak_base_url}/auth/admin/realms/{s.keycloak_realm}/clients',
+        headers={
+            'content-type': 'application/json',
+            'Authorization': f'bearer {token}'
+        }
+    ).json()
+
+
+def get_client() -> list:
+    return [client for client in get_clients() if s.keycloak_client_id == client['clientId']]
+
 
 def parse_result(action: str, result):
     try:
         if not result.ok:
             raise Exception(f'{action}: {result.text} {result.status_code}')
+        print(f'{action}: {result.text} {result.status_code}')
     except Exception as ex:
         pp(ex)
 
@@ -30,6 +67,16 @@ token = json.loads(requests.post(
         'password': s.keycloak_password
     }
 ).text)['access_token']
+
+# Delete existing realm if present
+if 1 == len(get_realm()):
+    api_result = requests.delete(
+        f'{s.keycloak_base_url}/auth/admin/realms/{s.keycloak_realm}',
+        headers={
+            'Authorization': f'bearer {token}'
+        }
+    )
+    parse_result('Delete realm', api_result)
 
 # Create realm
 api_result = requests.post(
@@ -46,13 +93,14 @@ api_result = requests.post(
 parse_result('Create realm', api_result)
 
 # Delete existing client if present
-api_result = requests.delete(
-    f'{s.keycloak_base_url}/auth/admin/realms/{s.keycloak_realm}/clients/{s.keycloak_client_id}',
-    headers={
-        'Authorization': f'bearer {token}'
-    }
-)
-parse_result('Delete client', api_result)
+if 1 == len(get_client()):
+    api_result = requests.delete(
+        f'{s.keycloak_base_url}/auth/admin/realms/{s.keycloak_realm}/clients/{s.keycloak_client_id}',
+        headers={
+            'Authorization': f'bearer {token}'
+        }
+    )
+    parse_result('Delete client', api_result)
 
 # Create client
 api_result = requests.post(
